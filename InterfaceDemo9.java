a97ec67f24ad39cabe97e2319816ca1b43c9fedd
Gajanan Sunil Dimble <gajanandimble2355@gmail.com>
2026-01-12 15:21:35 +0530

    Add files via upload

---
+++ InterfaceDemo9.java
@@ -0,0 +1,31 @@
+interface A 
+{
+    int no=11;
+    void fun();
+    private void Display()
+    {
+        System.out.println("Inside Private Display");
+        Display();
+    }
+    default void gun()
+    {
+        System.out.println("Inside gun");
+    }
+}
+class Demo implements A           
+{
+    public void fun()
+    {
+        System.out.println("Inside fun");         
+    }
+}
+class InterfaceDemo9
+{
+    public static void main(String A[])
+    {
+        Demo dobj=new Demo();
+        dobj.fun();
+        dobj.gun();
+        // dobj.Display();         Error
+    }
+}
