e92d588caa09ccdb1e82d455b636fa2306c2455e
Gajanan Sunil Dimble <gajanandimble2355@gmail.com>
2026-04-07 17:57:47 +0530

    Add files via upload

---
+++ File_IO in Java/program292.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class program292
+{
+    public static void main(String A[])
+    {
+        int Arr[]={45,21,90,54,78};
+        //thos is for each loop
+        for(int no:Arr)
+        {
+            System.out.println(no);
+        }
+    }
+}
